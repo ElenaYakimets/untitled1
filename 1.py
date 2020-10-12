298014275de6d8d91d050e9191622ac034fc39f8
Elena Yakimets <elenaxp@yandex.ru>
2020-10-12 22:58:56 +0300

    12

--- 1.py
+++ 1.py
@@ -1,3 +1,8 @@
 import openpyxl
 wb = openpyxl.reader.excel.load_workbook(filename="12.xlsx")
-print(wb.sheetnames)
+wb.active = 0
+sheet = wb.active
+#print (sheet['A1'].value)
+
+for i in range (1,12):
+    print(sheet['A'+str(i)].value, sheet['B'+str(i)].value, sheet['C'+str(i)].value)
