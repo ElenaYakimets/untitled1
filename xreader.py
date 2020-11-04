324ecf03f5b1fa8247a97c8d4802484b60894530
Elena Yakimets <elenaxp@yandex.ru>
2020-11-04 20:44:23 +0300

    12

--- xreader.py
+++ xreader.py
@@ -1,9 +1,9 @@
-from openpyxl import load_workbook, Workbook
+from openpyxl import load_workbook
 
 filename = "data.xlsx"
 
 if __name__ == "__main__":
-    wb: Workbook = load_workbook(filename)
+    wb = load_workbook(filename)
     sheet = wb.active
     c1 = sheet.cell(row=1, column=2)
     c1.value = 'B269OT177/BM032077'
