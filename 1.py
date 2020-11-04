951eda48c6292d40c4b8594e6d14ba28d4b52b40
Elena Yakimets <elenaxp@yandex.ru>
2020-11-04 20:15:47 +0300

    12

--- 1.py
+++ 1.py
@@ -1,3 +1,14 @@
-import openpyxl
-wb = openpyxl.reader.excel.load_workbook(filename="12.xlsx")
-print(wb.sheetnames)
+from openpyxl import load_workbook, Workbook
+
+filename = "data.xlsx"
+
+if __name__ == "__main__":
+    wb: Workbook = load_workbook(filename)
+    sheet = wb.active
+    c1 = sheet.cell(row=1, column=2)
+    c1.value = 'B269OT177/BM032077'
+    c1 = sheet.cell(row=2, column=2)
+    c1.value = '10013160/130920/0493176'
+        # sheet.title = 'Sheet1'
+        # print('sheet name is renamed as:'+ sheet.title)
+    wb.save(filename)
--- xreader.py
+++ xreader.py
@@ -1,7 +1,14 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 
 filename = "data.xlsx"
 
 if __name__ == "__main__":
-    wb = load_workbook(filename)
-    print(wb.sheetnames)
+    wb: Workbook = load_workbook(filename)
+    sheet = wb.active
+    c1 = sheet.cell(row=1, column=2)
+    c1.value = 'B269OT177/BM032077'
+    c1 = sheet.cell(row=2, column=2)
+    c1.value = '10013160/130920/0493176'
+        # sheet.title = 'Sheet1'
+        # print('sheet name is renamed as:'+ sheet.title)
+    wb.save(filename)
